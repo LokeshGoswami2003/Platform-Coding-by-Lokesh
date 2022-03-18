25c8f068dd8345e56fb9adf2d1001c73ec58c57f
LokeshGoswami2003 <lokesh.goswami.2003@gmail.com>
2022-03-18 10:10:39 +0530

    1st question on leetcode solved

---
+++ Leetcode_Problems/Two_Sum_1.java
@@ -0,0 +1,20 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                int temp = nums[i] + nums[j];
+                if (temp == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null;
+    }
+}
